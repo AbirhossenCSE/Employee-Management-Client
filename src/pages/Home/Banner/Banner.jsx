4ea87141a826ed91882a77e6d18ad95cd7bc53f1
Abir Hossen <abir.cse4@gmail.com>
2025-01-17 10:48:49 +0600

    banner

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+
+import img1 from '../../../assets/banner/01.jpg'
+import img2 from '../../../assets/banner/02.jpg'
+import img3 from '../../../assets/banner/03.png'
+
+
+const Banner = () => {
+    return (
+        <div>
+            <Carousel>
+                <div>
+                    <img src={img1} />
+                </div>
+                <div>
+                    <img src={img2} />
+                </div>
+                <div>
+                    <img src={img3} />
+                </div>
+            </Carousel>
+        </div>
+    );
+};
+
+export default Banner;
